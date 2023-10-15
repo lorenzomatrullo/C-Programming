69f7afffd9a365022aad071aa98cdf2cf5585ca2
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2023-10-15 15:12:51 +0200

    com

---
+++ FORWARD/for_loop.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+// for loop = repeats a section of code a limited amount of times
+
+
+int main() {
+
+    int i;
+
+    /*
+        1st statement = starting from
+        2nd statement = arriving to
+        3rd stament = whats the process between
+    */
+
+    for(i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+
+
+    return 0;
+}
