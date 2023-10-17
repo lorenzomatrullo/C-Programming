53b9800aca967e8c639011c264c70595e9a5cb3e
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2023-10-17 14:02:38 +0200

    comm

---
+++ FORWARD/math_operators.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/*
+    + (addition)
+    - (subtraction)
+    * (moltiplication)
+    / (division)
+    % (modulus) means  "the rest of a division"
+    ++ (increment)
+    -- (decrement)
+
+    x = x + 2
+    x += 2
+
+    x = x - 3
+    x -= 3
+
+    x = x * 4
+    x *= 4
+
+    x = x / 5
+    x /= 5
+
+    x = %2
+    x %= 2
+*/
+
+
+int main() {
+
+
+    return 0;
+}
