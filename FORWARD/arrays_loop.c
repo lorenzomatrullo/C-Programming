e8b55516158d492a6f4c4183e93b623d48fa1ce6
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2023-10-24 12:08:17 +0200

    array loop

---
+++ FORWARD/arrays_loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    double prices[] = {5.0, 10.0, 15.0, 20.0, 25.0};
+
+    /*
+    printf("%.1lf\n", prices[0]);
+    printf("%.1lf\n", prices[1]);
+    printf("%.1lf\n", prices[2]);
+    printf("%.1lf\n", prices[3]);
+    printf("%.1lf", prices[4]);
+    */
+
+   
+
+
+    return 0;
+}
