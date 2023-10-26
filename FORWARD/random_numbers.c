a9e808319ec9de4168ed6e3a8babd4d15c43cc5f
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2023-10-26 12:54:23 +0200

    random numbers

---
+++ FORWARD/random_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+// pseudo random numbers = A set of values or elements that are statistically random
+//                        (Don't use these for any sort of cryptographic security)
+
+
+int main(){
+
+    srand(time(0));
+
+    int number1 = (rand() % 6) + 1;
+    int number2 = (rand() % 6) + 1;
+    int number3 = (rand() % 6) + 1;
+
+    printf("%d\n", number1);
+    printf("%d\n", number2);
+    printf("%d", number3);
+
+    return 0;
+}
