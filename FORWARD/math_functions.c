41b2d0358f8c39f327ede5b38c14ed4a874edc10
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2023-10-30 12:39:42 +0100

    math functions

---
+++ FORWARD/math_functions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    double A = sqrt(9);             // square root of
+    double B = pow(2, 4);           // to the power of
+    int C = round(3.14);            // rounds a number
+    int D = ceil(3.14);             // rounds a number up
+    int E = floor(3.99);            // rounds a number down
+    double F = fabs(-100);          // absolute value of a number
+    double G = log(3);              // logarithm
+    double H = sin(45);             // sine
+    double I = cos(45);             // cosine
+    double J = tan(45);             // tangent
+    
+    printf("\n%lf", A);
+
+    return 0;
+}
