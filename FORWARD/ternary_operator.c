630c61fbaafa2b4e0b334f04ba3df56c89abef7c
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2023-10-30 12:33:50 +0100

    3 stuff

--- FORWARD/ternary_operator.c
+++ FORWARD/ternary_operator.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-// ternary operator = shortcut to if/else statement wwhen assigning/returning a value
+// ternary operator = shortcut to if/else statement when assigning/returning a value
 // ? = if true
 // : = if false
 
 
 int findMax(int x, int y) {
     return (x > y) ? x : y;
+    // if x > y return x, if false return y
 }
 
 int main() {
-    int max = findMax(5, 4);
+    int max = findMax(5, 4);        // arguments = passing two integers
     printf("%d", max);
 
     return 0;
