6d837151c0e1b813b18328a4787e759719251fb9
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2023-10-31 12:38:30 +0100

    writing files

--- FORWARD/writing_files.c
+++ FORWARD/writing_files.c
@@ -12,11 +12,41 @@
 
 int main(){
 
-    FILE *pF = fopen("C:\\Users\\Lorenzo\\Desktop\\text.txt", "w");
-
     
+    FILE *pF = fopen("C:\\Users\\Lorenzo\\Desktop\\text.txt", "a");
+
+    fprintf(pF, "\nPatrick");
+    printf("Data has been updated.");
 
     fclose(pF);
+
+
+    // RENAMING A FILE
+
+    /*
+    int renameFile;
+    char oldname[] = "C:\\Users\\Lorenzo\\Desktop\\text.txt";
+    char newname[] = "C:\\Users\\Lorenzo\\Desktop\\lol.txt";
+
+    renameFile = rename(oldname, newname);
+
+    if(renameFile == 0) {
+        printf("File renamed");
+    } else {
+        printf("No operation done");
+    }
+    */
+    
+
+    // REMOVING A FILE 
+
+   /*
+   if(remove("C:\\Users\\Lorenzo\\Desktop\\text.txt") == 0){
+    printf("That file was deleted successfully.");
+   } else {
+    printf("That file was NOT deleted.");
+   }
+   */
 
 
     return 0;
