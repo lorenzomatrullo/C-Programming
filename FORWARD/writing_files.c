184cf039b89ddb771e3a169ec2ffe09cacd29390
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2023-10-31 12:37:47 +0100

    commit

---
+++ FORWARD/writing_files.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+
+    2nd data:
+    a = append
+    w = write
+    r = read
+
+*/
+
+
+int main(){
+
+    FILE *pF = fopen("C:\\Users\\Lorenzo\\Desktop\\text.txt", "w");
+
+    
+
+    fclose(pF);
+
+
+    return 0;
+}
