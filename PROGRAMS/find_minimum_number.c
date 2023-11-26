9da7e2058aaff02ad5975412cea78d1848034ba8
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2023-11-26 15:36:04 +0100

    find minimum number

---
+++ PROGRAMS/find_minimum_number.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int find_min(int array[], int length);
+
+
+int main(){
+
+    int array1[] = {5, 9, 10, 11, 4, 3, 8, 6, 7};
+    int array1Length = sizeof(array1)/sizeof(array1[0]);
+
+    int min1 = find_min(array1, array1Length);
+
+    printf("Minimum: %d", min1);
+
+    return 0;
+}
+
+int find_min(int array[], int length){
+
+    int min;
+    min = array[0];
+
+    for(int i = 0; i < length; i++){
+        if(array[i] < min){
+            min = array[i];
+        }
+    }
+    
+    return min;
+}
