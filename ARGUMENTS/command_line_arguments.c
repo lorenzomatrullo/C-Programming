38b07002c4914c4be98c384c6f28ca239d2ea68b
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2023-12-06 18:28:24 +0100

    command line arguments

---
+++ ARGUMENTS/command_line_arguments.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// argc = arguments count
+// argv = arguments value
+// This allows you to work with inputs directly in cmd
+
+int main(int argc, int *argv[]){
+
+    if(argc != 3){
+        printf("Two args required!");
+        exit(-1);
+    }
+
+    int lower = atoi(argv[1]);
+    int higher = atoi(argv[2]);
+
+    for(int i = lower; i <= higher; i++){
+        printf("%d\n", i);
+    }
+
+
+
+    /*
+    printf("argc: %d\n", argc);
+
+    for(int i = 0; i < argc; i++){
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+    */
+
+    return 0;
+}
