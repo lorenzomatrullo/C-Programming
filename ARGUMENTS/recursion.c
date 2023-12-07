85202f6ea0b0e6a743516f8df0387f2135d41e2a
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2023-12-07 10:28:24 +0100

    recursion

---
+++ ARGUMENTS/recursion.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/*
+    Recursion is a way of solving a problem where our solution depends on 
+    solutions to smaller instances of the same problem
+
+    Practically this means we will write functions that call themselves!
+
+    Example: factorial problem
+
+    The factorial of n denoted by n! is the product of all positive integers <= n 
+
+    n! = n * (n-1) * (n-2) ... * 1
+
+    5! = 5 * 4 * 3 * 2 * 1 = 120
+*/
+
+int factorial(int);
+
+int main(){
+    
+    int n;
+    int fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    // iterative solution to computing factorial
+    /*
+        while(n <= 1){
+            factorial = n * factorial;
+            n--;
+        }
+    */
+
+   // recursive solution
+   fact = factorial(n);
+
+   printf("factorial: %d", fact);
+
+}
+
+int factorial(int n){
+
+    if(n == 1) return 1;
+
+    return n * factorial(n - 1);
+}
