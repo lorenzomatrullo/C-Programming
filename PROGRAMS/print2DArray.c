304d22774f73c64e6ce773db990dd55cff10a69d
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2023-12-13 19:38:57 +0100

    printing 2d arrays exercise

--- PROGRAMS/print2DArray.c
+++ PROGRAMS/print2DArray.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
-#define ROWS 4
-#define COLS 5
+#define ROWS 3
+#define COLS 3
+
+int print2DArray(int [ROWS][COLS]);
+
+int main(){
+
+    int list[ROWS][COLS] = {
+        {1, 2, 3,},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    print2DArray(list);
+
+    return 0;
+}
 
 int print2DArray(int array[ROWS][COLS]){
     for(int i = 0; i < ROWS; i++){
         for(int j = 0; j < COLS; j++){
             printf("%4d ", array[i][j]);
         }
-        printf("\n");
+        printf("\n"); // after every row ended there is gonna be a new line.
     }
-}
-
-int main(){
-
-    int array[ROWS][COLS] = {
-        {1, 2, 3, 4, 5},
-        {6, 7, 8, 9, 10},
-        {11, 12, 13, 14, 15},
-        {16, 17, 18, 19, 20}
-    };
-
-    print2DArray(array);
-
-    return 0;
 }
