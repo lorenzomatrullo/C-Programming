b96e7afcc1b48e90e8ec20e3cea9eea307e7a3bd
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2023-12-14 10:16:25 +0100

    generate random numbers from  1 to 100

---
+++ PROGRAMS/generate_nums.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+    srand(time(0));
+
+    for(int i = 0; i < 100; i++){
+        int num = rand() % 100 + 1;
+        printf("num[%d] = %d\n", i + 1, num);
+    }
+   
+
+    return 0;
+}
