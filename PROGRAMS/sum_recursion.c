086e346622ff8fc6bade25adb462439dc9f5d3fb
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2024-03-05 11:44:50 +0100

    formatting

--- PROGRAMS/sum_recursion.c
+++ PROGRAMS/sum_recursion.c
@@ -3,25 +3,24 @@
 // Recursive function to calculate the sum of numbers in an array
 int sumNumbersRecursive(int [], int);
 
-
 int main() {
-    int numbers[] = {1, 2, 3, 4, 5};
-    int length = sizeof(numbers) / sizeof(numbers[0]);
+    int array[] = {1, 2, 3, 4, 5};
+    int array_length = sizeof(array)/sizeof(array[0]);
+
 
     // Call the recursive function to calculate the sum of numbers
-    int result = sumNumbersRecursive(numbers, length);
-
+    int result = sumNumbersRecursive(array, array_length);
     printf("The sum of numbers is: %d\n", result);
 
     return 0;
 }
 
 
-int sumNumbersRecursive(int numbers[], int length) {
+int sumNumbersRecursive(int array[], int length) {
     // Base case: if the length is 0, return 0
     if (length == 0) {
         return 0;
     }
     // Recursive case: sum the current element with the sum of the rest of the array
-    return numbers[length - 1] + sumNumbersRecursive(numbers, length - 1);
+    return array[length - 1] + sumNumbersRecursive(array, length - 1);
 }
