15836030552e694c3b7c487505e11426ed81432d
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2024-03-05 11:43:10 +0100

    formatting

--- PROGRAMS/sum_array.c
+++ PROGRAMS/sum_array.c
@@ -4,12 +4,12 @@ int sum(int[], int);
 
 int main(){
 
-    int array1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int array1_size = sizeof(array1)/sizeof(array1[0]);
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int array_length = sizeof(array)/sizeof(array[0]);
 
-    int array1_sum = sum(array1, array1_size);
 
-    printf("Array1 sum: %d", array1_sum);
+    int result = sum(array, array_length);
+    printf("Array1 sum: %d", result);
 
     return 0;
 }
@@ -21,6 +21,6 @@ int sum(int array[], int length){
     for(int i = 0; i < length; i++){
         sum = sum + array[i];        
     }
-    return sum;
 
+    return sum;
 }
