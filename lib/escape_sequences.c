afe483ce9a6a58d02ec707d72214cabd7dbec8e2
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2024-04-21 17:45:29 +0200

    formatting

--- lib/escape_sequences.c
+++ lib/escape_sequences.c
@@ -23,14 +23,6 @@ int main(){
     printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
     printf("\"I like Pizza\" -Abraham Lincoln probably");
 
-    char full_name[100];
-    printf("\nEnter your full name: ");
-
-    // scanf("%s", full_name);                                               // will only display the first word
-    // scanf("%[^\n]", full_name);                                           // will display the entire string
-    fgets(full_name, sizeof(full_name)/sizeof(full_name[0]), stdin);         // will display the entire string
-
-    printf("You entered: %s\n", full_name);
 
     return 0;
 }
